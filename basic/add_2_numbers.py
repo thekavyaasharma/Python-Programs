7bff56370aacd76ac9e4a922d4aed9848c98be11
kavya <kavyash1804@gmail.com>
2026-04-26 21:43:09 +0530

    new file added

---
+++ basic/add_2_numbers.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+
+def add_numb(a,b):
+    return a+b
+print(add_numb(a,b))
+
+#a+b 
+print(eval(input("Enter calculation: ")))
+
