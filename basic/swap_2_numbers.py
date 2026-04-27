d9f26822d4ddac02bf19e2e07c554dfac987176c
kavya <kavyash1804@gmail.com>
2026-04-27 23:56:18 +0530

    new prog

--- basic/swap_2_numbers.py
+++ basic/swap_2_numbers.py
@@ -5,3 +5,12 @@ a=b
 b=temp 
 print(f"using temp variable.\n A: {a}, B:{b}")
 
+# using add and subtrat 
+print("\n using addition and subtraction method")
+a,b = int(input("ENTER A: ")), int(input("ENTER B: "))
+
+a = a-b  # 10 - 200 -> -190
+b = b+a # 200- 190 = 10
+a = b-a # 10 - (-190) = 200
+
+print(f'A : {a}, B : {b}')
