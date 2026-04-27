e086dd187354ee6740455896623a0465fa8e5003
kavya <kavyash1804@gmail.com>
2026-04-28 00:40:17 +0530

    recursion to rev string

--- basic/reverse_string.py
+++ basic/reverse_string.py
@@ -10,3 +10,13 @@ for ch in user_input:
 print(f'String: {user_input} \nReversed string: {rev}')
 
 
+# using recursion: fxn calling itself until base condn satisfied
+def rev_string(s):
+    if len(s) ==1 :
+        return s
+    return rev_string(s[1:])+s[0]
+
+st = input("ENTER STRING TO REVERSE USING RECURSION: ")
+rev = rev_string(st)
+print(f"Reversed String is: {rev}")
+
