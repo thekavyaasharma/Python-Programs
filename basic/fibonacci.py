47714ea84f0cd51f59603fc37357d35ba94fd980
kavya <kavyash1804@gmail.com>
2026-05-06 00:59:32 +0530

    fibonacci using recursion

--- basic/fibonacci.py
+++ basic/fibonacci.py
@@ -5,12 +5,58 @@ n = int(input("Enter a number: "))
 a = 0
 b = 1
 print(a,b,end=" ")
-i = 0
-while i < n:
+
+for _ in range(n-1):
     c = a+b
     a,b = b,c
     print(c, end=" ")
-    i+=1
+    
+print()
+
+'''
+           fib(4)
+          /      \
+      fib(3)  +  fib(2)
+      /    \      /    \
+  fib(2) + fib(1)fib(1) + fib(0)
+  /    \
+fib(1)+fib(0)
+
+'''
+
+# function 
+def fibonacci(n):
+    if n <=1:
+        return n
+    a = 0
+    b = 1
+    for _ in range(n):
+        a,b = b, a+b
+
+    return a
+
+res = fibonacci(n)
+print("Using simple function to print fibonacci series: ", res )
+
+
+
+
+
+
+
+
+# recursion
+def fib_rec(n):
+    if n <=1:
+        return n
+    
+    return fib_rec(n-1) + fib_rec(n-2)
+
+res = fib_rec(n)
+print("Using rescursion: ", res)
+
+
+
 
 
 
