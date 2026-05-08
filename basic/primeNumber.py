302d8fb62491ebce768504d36a044c39057fad3d
kavya <kavyash1804@gmail.com>
2026-05-08 21:44:49 +0530

    prime numbers using recursion

--- basic/primeNumber.py
+++ basic/primeNumber.py
@@ -19,3 +19,21 @@ else:
     print(f"{number} is a Prime number.")
 
     
+print()
+print("Using recursion ")
+
+def check_prime(n,i=2):
+
+    # base cases
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % i ==0:
+        return False
+    if i * i > n:
+        return True
+
+    return check_prime(n,i+1)
+
+print("Prime" if check_prime(number)== True else "Composit")
