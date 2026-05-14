92a8dc2ceb726801251855863e17c1d5b64276b8
kavya <kavyash1804@gmail.com>
2026-05-14 23:01:51 +0530

    added method 2

--- intermediate/separate-digits-in-array.py
+++ intermediate/separate-digits-in-array.py
@@ -15,3 +15,23 @@ nums = [1,22,98,1867]
 print(f'original array: \n{nums}')
 print('Separated digits in array: ')
 print(separate_digits(nums))
+
+def sep2(num):
+    n = len(num)
+    result = []
+
+    for i in range(n-1,-1,-1):
+        x = num[i]
+        while x!=0:
+            result.append(x%10)
+            x //=10
+
+    return result[::-1]
+                
+    
+    
+nums = [1,22,98,1867]
+
+y = sep2(nums)
+
+print(y)
