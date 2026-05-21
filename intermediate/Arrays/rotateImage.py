08bdedde5c21a25b623962f2ef793b59746ab364
kavya <kavyash1804@gmail.com>
2026-05-22 01:24:15 +0530

    LC48

--- intermediate/Arrays/rotateImage.py
+++ intermediate/Arrays/rotateImage.py
@@ -33,3 +33,30 @@ def rotate_image(matrix):
     return matrix
 
 print(rotate_image([[1,2,3],[4,5,6],[7,8,9]]))
+
+# without using built in 
+def rotate_image(matrix):
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i+1,n):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+        
+    for i in range(n):
+        #columns
+        left = 0
+        right = n-1
+
+        while(left<right):
+            temp = matrix[i][left]
+            matrix[i][left] = matrix[i][right]
+            matrix[i][right] = temp
+
+            left +=1
+            right -=1
+
+    return matrix
+
+print(rotate_image([[1,2,3],[4,5,6],[7,8,9]]))
