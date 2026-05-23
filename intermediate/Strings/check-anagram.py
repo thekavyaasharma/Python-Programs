738a195b3b4a1d43c2d5da6c53a4644f8c2b1c29
kavya <kavyash1804@gmail.com>
2026-05-24 00:47:23 +0530

    valid-anagram

--- intermediate/Strings/check-anagram.py
+++ intermediate/Strings/check-anagram.py
@@ -2,6 +2,8 @@
 242. Valid Anagram
 Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
+An anagram is a word or phrase formed by rearranging the letters of a different word or
+phrase, using all the original letters exactly once.
  
 
 Example 1:
@@ -17,4 +19,91 @@ Input: s = "rat", t = "car"
 Output: false
 
 '''
+
+# using hashmap 
+
+def anagram(s,t):
+    if len(s) != len(t):
+        return False
     
+    hmap = {}
+    x = set(s)
+    for i in x:
+        hmap[i] = s.count(i)
+
+    for i in t:
+        if i not in s :
+            return False
+            break
+        elif hmap[i] != t.count(i):
+            return False
+            break
+    else:
+        return True
+
+s = "anagram"
+t = "nagaram"
+
+print(anagram(s,t))
+
+
+s = "rat"
+t = "car"
+
+print(anagram(s,t))
+
+
+s = ""
+t = "hi"
+print(anagram(s,t))
+
+s = "race"
+t = "care"
+print(anagram(s,t))
+
+s = "ab"
+t = "a"
+print(anagram(s,t))
+
+
+
+# using pythonic way 
+def anagram(s,t):
+    if len(s) != len(t):
+        return False
+    
+    x = set(s)
+    for i in s:
+        if i not in t or s.count(i) != t.count(i):
+            return False
+            break
+    else:
+        return True
+    
+print()
+s = "anagram"
+t = "nagaram"
+
+print(anagram(s,t))
+
+
+s = "rat"
+t = "car"
+
+print(anagram(s,t))
+
+
+s = ""
+t = "hi"
+print(anagram(s,t))
+
+s = "race"
+t = "care"
+print(anagram(s,t))
+
+s = "ab"
+t = "a"
+print(anagram(s,t))
+
+
+
