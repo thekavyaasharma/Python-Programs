e95d097446462ce245f2b83d283e515fa8dd6372
kavya <kavyash1804@gmail.com>
2026-05-26 01:40:26 +0530

    hollow triangle pattern

--- basic/hollowTriangle.py
+++ basic/hollowTriangle.py
@@ -4,4 +4,34 @@
    *   *
   *     *
  * * * * * 
+
 '''
+
+n = 5
+for i in range(n,-1,-1):
+    print(" " * i, end = "")
+    for j in range(n-i):
+        print("* ", end = "")
+
+    print()
+
+
+
+'''
+
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+
+'''
+n = 5
+for i in range(n,-1,-1):
+    print(" " * i, end = "")
+    for j in range(n-i):
+        print("*", end = " ")
+
+    print()
+
+
