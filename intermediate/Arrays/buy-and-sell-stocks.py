43b59cb31c159d7213ea0b9cbab97247b4283651
kavya <kavyash1804@gmail.com>
2026-05-28 00:01:35 +0530

    LC-21

--- intermediate/Arrays/buy-and-sell-stocks.py
+++ intermediate/Arrays/buy-and-sell-stocks.py
@@ -1,7 +1,8 @@
 '''
 You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
-You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future
+ to sell that stock.
 
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
@@ -19,3 +20,21 @@ Input: prices = [7,6,4,3,1]
 Output: 0
 Explanation: In this case, no transactions are done and the max profit = 0.
 '''
+
+def buyStock(prices):
+    buyPrice = prices[0]
+    profit = 0
+
+    for i in prices:
+        if buyPrice > i:
+            buyPrice = i
+        
+        profit = max(profit, i - buyPrice)
+
+    return profit 
+
+prices1 = [7,1,5,3,6,10] # 9
+prices2 = [7,1,5,3,6,4] # 5
+
+print(buyStock(prices1))
+print(buyStock(prices2))
