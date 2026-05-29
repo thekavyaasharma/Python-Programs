ea0ca2803b727844e837cec0f8f24d5f56497ad1
kavya <kavyash1804@gmail.com>
2026-05-29 22:44:58 +0530

    Pattern Program 1

--- basic/hollowTriangle.py
+++ basic/hollowTriangle.py
@@ -8,12 +8,12 @@
 '''
 
 n = 5
-for i in range(n,-1,-1):
-    print(" " * i, end = "")
-    for j in range(n-i):
-        print("* ", end = "")
-
-    print()
+for i in range(1,n+1):
+    for j in range(0,i*2):
+        print(" ", end = "")
+        for k in range(0,i*2-1):
+            print("*", end = "")
+        print()
 
 
 
