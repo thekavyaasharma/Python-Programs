10a2c4cf2680a4002e31a8b9571cdd4b7bf24faa
kavya <kavyash1804@gmail.com>
2026-06-17 00:01:12 +0530

    LEETCODE-3612-MED

---
+++ intermediate/Strings/process-string-with-special-operations-i.py
@@ -0,0 +1,17 @@
+# 3612 - medium - Process String with Special Operations I
+class Solution:
+    def processStr(self, s: str) -> str:
+        result = []
+
+        for ch in s:
+            if ch.islower():
+                result.append(ch)
+            elif ch == '#':
+                result.extend(result)
+            elif ch == '%':
+                result.reverse()
+            elif ch == '*' and len(result)!=0:
+                result.pop()
+
+        
+        return "".join(result
