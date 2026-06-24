19e64c9d12d71b3c2fc2fa419c34622111a3b5ca
kavya <kavyash1804@gmail.com>
2026-06-25 02:03:27 +0530

    LEETCODE-83-EASY

---
+++ intermediate/Linked List/remove-duplicates-from-sorted-list.py
@@ -0,0 +1,13 @@
+# 83. Remove Duplicates from Sorted List - EASY
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        res = head 
+
+        while head and head.next :
+            if head.val == head.next.val :
+                head.next = head.next.next
+            else:
+                head = head.next
+        
+        return res 
+        
