af9c4ffdd5663a943de99cbfa85fa09b99f65777
kavya <kavyash1804@gmail.com>
2026-07-01 02:00:32 +0530

    LEETCODE-1358-MED

---
+++ intermediate/Arrays/two pointer/number-of-substrings-containing-all-three-characters.py
@@ -0,0 +1,15 @@
+# 1358. Number of Substrings Containing All Three Characters - Medium
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        freq = [0] * 3
+        ans = 0
+        n = len(s)
+        left = 0
+
+        for right in range(n):
+            freq[ord(s[right]) - ord('a')] +=1
+            while(freq[0] and freq[1] and freq[2]):
+                ans += n - right
+                freq[ord(s[left]) - ord('a')] -=1
+                left +=1
+        return ans 
