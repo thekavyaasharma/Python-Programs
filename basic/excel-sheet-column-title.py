22d7b9a459c464c0e9a5ade220914aa60ba15665
kavya <kavyash1804@gmail.com>
2026-08-10 01:24:59 +0530

    LEETCODE-168-EASY

---
+++ basic/excel-sheet-column-title.py
@@ -0,0 +1,11 @@
+# 168. Excel Sheet Column Title - Easy
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        res = ""
+        while columnNumber >0:
+            columnNumber -=1
+            res = chr(columnNumber%26 + ord('A')) + res
+            columnNumber//=26
+        return res
+
+        
